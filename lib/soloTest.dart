430b01e3cdb80e2ce6c23a86764d9a5566ba384f
ayomicahyo <62920255+ayomicahyo@users.noreply.github.com>
2020-11-17 18:42:44 +0700

    initializeMain+soloTest v0
    Menyiapkan main.dart dan connector ke page SoloTest.dart

---
+++ lib/soloTest.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Solotest extends StatefulWidget {
+  @override
+  _SolotestState createState() => _SolotestState();
+}
+
+class _SolotestState extends State<Solotest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
